edfd68d90846f48dd8bbfbe408eb46bcb4341baf
Hhyemm <hyem0127@gmail.com>
2022-06-01 02:43:22 +0900

    Add : 망고픽 잇딜 화면

--- mangoplate/mangoplate/Sources/Cells/MangoPick/EatDealCell.swift
+++ mangoplate/mangoplate/Sources/Cells/MangoPick/EatDealCell.swift
@@ -1,10 +1,20 @@
 
 import UIKit
 
-class EatDealCell: UICollectionViewCell {
+class EatDealCell: UITableViewCell {
 
+    @IBOutlet weak var discountView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        discountView.layer.cornerRadius = 5
+        discountView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
     }
 
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
 }
--- mangoplate/mangoplate/Sources/ViewControllers/MangoPick/EatDealViewController.swift
+++ mangoplate/mangoplate/Sources/ViewControllers/MangoPick/EatDealViewController.swift
@@ -4,11 +4,14 @@ import UIKit
 class EatDealViewController: UIViewController {
 
     @IBOutlet weak var regionSelectView: UIView!
+    @IBOutlet weak var surroundView: UIView!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setViewDesign()
+        setTableView()
         
     }
     
@@ -16,5 +19,30 @@ class EatDealViewController: UIViewController {
         regionSelectView.layer.borderWidth = 1
         regionSelectView.layer.borderColor = UIColor.mainOrangeColor.cgColor
         regionSelectView.layer.cornerRadius = regionSelectView.frame.height / 2
+        surroundView.layer.cornerRadius = surroundView.frame.height / 2
+    }
+    
+    func setTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        let nib = UINib(nibName: "EatDealCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "EatDealCell")
+    }
+}
+
+extension EatDealViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EatDealCell") as! EatDealCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
