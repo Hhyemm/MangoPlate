4d2c533fd79c7de6408717d8709cc936d4cbb2c4
Hhyemm <hyem0127@gmail.com>
2022-06-01 12:17:12 +0900

    Add : 소식 홀릭 화면

---
+++ mangoplate/mangoplate/Sources/ViewControllers/News/HolicViewController.swift
@@ -0,0 +1,75 @@
+
+import UIKit
+
+class HolicViewController: UIViewController {
+
+    @IBOutlet weak var select5View: UIView!
+    @IBOutlet weak var select3View: UIView!
+    @IBOutlet weak var select1View: UIView!
+    
+    @IBOutlet weak var select5Label: UILabel!
+    @IBOutlet weak var select3Label: UILabel!
+    @IBOutlet weak var select1Label: UILabel!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setViewDesign(select5View)
+        setViewDesign(select3View)
+        setViewDesign(select1View)
+        pressSelectButton(select5View, select5Label)
+        upPressSelectButton(select3View, select3Label)
+        upPressSelectButton(select1View, select1Label)
+        
+        setCollectionVieW()
+    }
+    
+    func setViewDesign(_ selectView: UIView) {
+        selectView.layer.borderWidth = 1
+        selectView.layer.cornerRadius = selectView.frame.height / 2
+    }
+    
+    func pressSelectButton(_ selectView: UIView, _ selectLabel: UILabel) {
+        selectView.layer.borderColor = UIColor.mainOrangeColor.cgColor
+        selectLabel.textColor = .mainOrangeColor
+    }
+    
+    func upPressSelectButton(_ selectView: UIView, _ selectLabel: UILabel) {
+        selectView.layer.borderColor = UIColor.mainLightGrayColor.cgColor
+        selectLabel.textColor = .mainLightGrayColor
+        
+    }
+    
+    func setCollectionVieW() {
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        collectionView.register(UINib(nibName: "HolicCell", bundle: nil), forCellWithReuseIdentifier: "HolicCell")
+        collectionView.register(UINib(nibName: "NewsCell", bundle: nil), forCellWithReuseIdentifier: "NewsCell")
+    }
+}
+
+extension HolicViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.item == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HolicCell", for: indexPath) as! HolicCell
+            return cell
+        }
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NewsCell", for: indexPath) as! NewsCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.item == 0 {
+            return CGSize(width: collectionView.frame.width, height: 55)
+        }
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height/1.3)
+    }
+}
+
